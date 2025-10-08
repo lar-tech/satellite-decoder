a21d264e1335038a6ff7232f38acbfb08508e036
Ramin Leon Neymeyer <ramin.leon.neymeyer@gmail.com>
2025-10-08 11:10:43 +0200

    found correct constellation

--- main.m
+++ main.m
@@ -3,19 +3,33 @@ clear; close all; clc;
 %% parameter
 % file
 Data.filePath = "data/input.wav";   % raw IQ-samples
-Data.minDataIdx = 500000;           % min position of samples
-Data.maxDataIdx = 600000;           % max position of samples
+Data.minDataIdx = 1;           % min position of samples
+Data.maxDataIdx = 500000;           % max position of samples
 
 % general
 Params.minClip = -0.01;             % min value for clipping
 Params.maxClip = 0.01;              % max value for clippling
 Params.M = 4;                       % mumber of symbols
+Params.constellation = [3 2 0 1];   % constellation of gray-coding
 Params.symbolRate = 72e3;           % symbolrate
 Params.targetSps = 4;               % target samples per symbol
 
 % rcc
 Rcc.rollOff = 0.35;                 % roll-off-factor
 Rcc.spanSym = 10;                   % window length
+
+% sync
+Sync.testConstellations = true;    %
+Sync.constellations = {
+                        [0 1 3 2], ...      % default
+                        [1 3 2 0], ...      % 90°
+                        [3 2 0 1], ...      % 180°
+                        [2 0 1 3], ...      % 270°
+                        [2 3 1 0], ...      % Imag-inverted
+                        [1 0 2 3], ...      % Re-inverted
+                        [0 2 3 1], ...      % (I, Q) -> (Q, I)
+                        [3 1 0 2], ...      % (I, Q) -> (-Q, -I)
+                    };
 
 % viterbi
 Viterbi.codeRate = 1/2;             % Code rate of convolutional encoder
@@ -26,83 +40,30 @@ Viterbi.softInputWordLength = 8;    % soft-input-word-length
 
 %% demodulate qpsk
 [symbols, fsResampled] = demod(Data, Params, Rcc);
-scatterplot(symbols);
+% scatterplot(symbols);
 
 %% sync data
-% constellation rotation and symmetry
-constellations = {
-    [0 1 3 2], ...      % default
-    [1 3 2 0], ...      % 90°
-    [3 2 0 1], ...      % 180°
-    [2 0 1 3], ...      % 270°
-    [2 3 1 0], ...      % Imag-inverted
-    [1 0 2 3], ...      % Re-inverted
-    [0 2 3 1], ...      % (I, Q) -> (Q, I)
-    [3 1 0 2], ...      % (I, Q) -> (-Q, -I)
-};
+symbols = sync(symbols, Sync, Params, Viterbi);
 
-% frame-synchronization-word
-syncHex = '1ACFFC1D';
-syncBytes = sscanf(syncHex, '%2x').';
-syncBits = de2bi(syncBytes, 8, 'left-msb');
-syncBits = reshape(syncBits.', 1, []);
-
-% viterbi-decoder
-trellis = poly2trellis(Viterbi.constLen, Viterbi.codeGenPoly);
-
-%% soft decoding
-vDecSoft = comm.ViterbiDecoder( ...
-        'TrellisStructure', trellis, ...
-        'InputFormat', 'Soft',...
-        'TracebackDepth', Viterbi.tblen...
-        );
-
-for i=1:numel(constellations)
-    % qpsk demodulation
-    softBits = pskdemod( ...
-                symbols, ...
-                Params.M, ...
-                pi/4, ...
-                constellations{i}, ...
-                OutputType="llr" ...
-                ); % PlotConstellation=true
-    softBitsScaled = softBits * 8;
-    
-    % viterbi-decoder
-    reset(vDecSoft);
-    decBitsSoft = vDecSoft(softBitsScaled);
-    
-    % cross-correlation
-    [corr,lags] = xcorr(decBitsSoft, syncBits);
-    
-    figure(i);
-    plot(corr);
-end
-
-%% hard decoding
-vDecHard = comm.ViterbiDecoder( ...
-        'TrellisStructure', trellis, ...
-        'InputFormat', 'Hard',...
-        'TracebackDepth', Viterbi.tblen...
-        );
-for i=1:numel(constellations)
-    % qpsk demodulation hard
-    hardBits = pskdemod( ...
-                symbols, ...
-                Params.M, ...
-                pi/4, ...
-                constellations{i}...
-                ); % PlotConstellation=true
-    hardBits = de2bi(hardBits, 2, 'left-msb');
-    hardBits = reshape(hardBits.', [], 1);
-
-    % viterbi-decoder
-    reset(vDecHard);
-    decBitsHard = vDecHard(hardBits);
-
-    % cross-correlation
-    [corr,lags] = xcorr(decBitsHard, syncBits);
-
-    figure(8+i);
-    plot(corr);
-end
+% % frame-synchronization-word
+% syncHex = 'FCA2B63DB00D9794';
+% syncBytes = sscanf(syncHex, '%2x').';
+% syncBits = de2bi(syncBytes, 8, 'left-msb');
+% syncBits = reshape(syncBits.', 1, []);
+% syncBits = 2*double(syncBits)-1;
+% 
+% % qpsk demodulation hard
+% hardBits = pskdemod( ...
+%             symbols, ...
+%             Params.M, ...
+%             pi/4, ...
+%             Params.constellation...
+%             ); 
+% hardBits = de2bi(hardBits, 2, 'left-msb');
+% hardBits = reshape(hardBits.', [], 1);
+% 
+% % cross-correlation
+% [corr,lags] = xcorr(hardBits, syncBits);
+% 
+% figure();
+% plot(corr);
--- sync.m
+++ sync.m
@@ -1,3 +1,35 @@
-function sync()
+function symbols = sync(symbols, Sync, Params, Viterbi)
+    
+    % tests different constellations 
+    if Sync.testConstellations == true
+        % sync word before using viterbi
+        syncHex = 'FCA2B63DB00D9794';
+        syncBytes = sscanf(syncHex, '%2x').';
+        syncBits = de2bi(syncBytes, 8, 'left-msb');
+        syncBits = reshape(syncBits.', 1, []);
+        syncBits = 2*double(syncBits)-1;
 
-end 
+        % constellation rotation and symmetry
+        for i=1:length(Sync.constellations)
+
+            % qpsk demodulation hard-decoding
+            hardBits = pskdemod( ...
+                        symbols, ...
+                        Params.M, ...
+                        pi/4, ...
+                        Sync.constellations{i}...
+                        ); 
+            hardBits = de2bi(hardBits, 2, 'left-msb');
+            hardBits = reshape(hardBits.', [], 1);
+            
+            % cross-correlation
+            [corr,~] = xcorr(hardBits, syncBits);
+            
+            figure(i);
+            plot(corr);
+            xlabel("Sample");
+            ylabel("Amplitude")
+            title(sprintf("Crosscorrelation of FCA2B63DB00D9794 and %s", mat2str(Sync.constellations{i})));
+        end
+    end
+end
