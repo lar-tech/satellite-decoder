863ca2266f3f00708b54f3e32e51f74fad6bb9c0
Ramin Leon Neymeyer <ramin.leon.neymeyer@gmail.com>
2025-10-14 16:14:00 +0200

    minor changes

--- main.m
+++ main.m
@@ -37,9 +37,10 @@ Viterbi.softInputWordLength = 8;    % soft-input-word-length
 
 % descrambler
 Descrambler.base = 2;                           % binary base
-% Descrambler.polynom = '1+x^-3+x^-5+x^-7+x^-8';  % polynom
-Descrambler.polynom = '1+x^-14+x^-17';  % polynom
-Descrambler.init = [1 1 0 0 0 1 1 1 0 0 0 1 1 1 0 0 0];                   % initial conditions
+Descrambler.polynom = '1+x^-3+x^-5+x^-7+x^-8';  % polynom
+Descrambler.init = ones(1,8);                   % initial conditions
+% Descrambler.polynom = '1+x^-14+x^-17';  
+% Descrambler.init = [1 1 0 0 0 1 1 1 0 0 0 1 1 1 0 0 0];
 
 % reed-solomon
 ReedSolomon.interleavingDepth = 4;
@@ -52,7 +53,7 @@ ReedSolomon.primitivePolynomial = [1 1 0 0 0 0 1 1 1];  % x^8 + x^7 + x^2 + x + 
 
 %% find constellation
 [softBits, constellation] = constellation(symbols, Params);
-% 
+
 %% decoding and descrambling
 [cadus, cvcdus] = decode(softBits, Viterbi, Descrambler, Params);
 
@@ -66,55 +67,55 @@ for i = 1:numel(cadus)
     cvcdu = reshape(cvcdus{i}, 8, []).';
     cvcdus{i} = bi2de(cvcdu, 'left-msb');
 end
-
-% de-interleaving
-deinterleavedBlocks = cell(1, numel(cvcdus));
-for i = 1:numel(cvcdus)
-    cvcdu = cvcdus{i};
-    rsBlock = zeros(ReedSolomon.interleavingDepth, ReedSolomon.codeWordLength, 'uint8');
-    for j = 1:ReedSolomon.interleavingDepth
-        rsBlock(j, :) = cvcdu(j:ReedSolomon.interleavingDepth:end);
-    end
-    deinterleavedBlocks{i} = rsBlock;
-end
-
-% reed-solomon decoder
-generatorPolynomial = rsgenpoly(ReedSolomon.codeWordLength, ReedSolomon.messageLength, bi2de(ReedSolomon.primitivePolynomial, 'left-msb'));
-rsDec = comm.RSDecoder( ...
-            'CodewordLength', ReedSolomon.codeWordLength, ...    
-            'MessageLength', ReedSolomon.messageLength, ...     
-            'GeneratorPolynomialSource', 'Property', ...
-            'GeneratorPolynomial', generatorPolynomial, ...
-            'PrimitivePolynomialSource', 'Property', ...
-            'PrimitivePolynomial', ReedSolomon.primitivePolynomial ...
-        );
-correctedBlocks = cell(1, numel(deinterleavedBlocks));
-numErrors = cell(1, numel(deinterleavedBlocks));
-for i = 1:numel(deinterleavedBlocks)
-    rsBlock = deinterleavedBlocks{i};
-    correctedBlock = zeros(4, ReedSolomon.messageLength, 'uint8');
-    numError = zeros(4,1);
-    for j = 1:4
-        [correctedBlock(j,:), numError(j)] = rsDec(rsBlock(j,:).');
-        fprintf('Block %d: %d Bytefehler korrigiert\n', j, numError(j));
-    end
-    correctedBlocks{i} = correctedBlock;
-    numErrors{i} = numError;
-end
-
-% re-interleaving
-reinterleavedBlocks = cell(1, numel(correctedBlocks));
-for i = 1:numel(correctedBlocks)
-    correctedBlock = correctedBlocks{i};
-    reinterleavedBlock = zeros(1, ReedSolomon.interleavingDepth*ReedSolomon.messageLength, 'uint8');
-    idx = 1;
-    for col = 1:ReedSolomon.messageLength
-        for row = 1:ReedSolomon.interleavingDepth
-            reinterleavedBlock(idx) = correctedBlock(row, col);
-            idx = idx + 1;
-        end
-    end
-    bits = de2bi(reinterleavedBlock, 8, 'left-msb');
-    bits = reshape(bits.', 1, []);
-    reinterleavedBlocks{i} = logical(bits(:));
-end
+% 
+% % de-interleaving
+% deinterleavedBlocks = cell(1, numel(cvcdus));
+% for i = 1:numel(cvcdus)
+%     cvcdu = cvcdus{i};
+%     rsBlock = zeros(ReedSolomon.interleavingDepth, ReedSolomon.codeWordLength, 'uint8');
+%     for j = 1:ReedSolomon.interleavingDepth
+%         rsBlock(j, :) = cvcdu(j:ReedSolomon.interleavingDepth:end);
+%     end
+%     deinterleavedBlocks{i} = rsBlock;
+% end
+% 
+% % reed-solomon decoder
+% generatorPolynomial = rsgenpoly(ReedSolomon.codeWordLength, ReedSolomon.messageLength, bi2de(ReedSolomon.primitivePolynomial, 'left-msb'));
+% rsDec = comm.RSDecoder( ...
+%             'CodewordLength', ReedSolomon.codeWordLength, ...    
+%             'MessageLength', ReedSolomon.messageLength, ...     
+%             'GeneratorPolynomialSource', 'Property', ...
+%             'GeneratorPolynomial', generatorPolynomial, ...
+%             'PrimitivePolynomialSource', 'Property', ...
+%             'PrimitivePolynomial', ReedSolomon.primitivePolynomial ...
+%         );
+% correctedBlocks = cell(1, numel(deinterleavedBlocks));
+% numErrors = cell(1, numel(deinterleavedBlocks));
+% for i = 1:numel(deinterleavedBlocks)
+%     rsBlock = deinterleavedBlocks{i};
+%     correctedBlock = zeros(4, ReedSolomon.messageLength, 'uint8');
+%     numError = zeros(4,1);
+%     for j = 1:4
+%         [correctedBlock(j,:), numError(j)] = rsDec(rsBlock(j,:).');
+%         fprintf('Block %d: %d Bytefehler korrigiert\n', j, numError(j));
+%     end
+%     correctedBlocks{i} = correctedBlock;
+%     numErrors{i} = numError;
+% end
+% 
+% % re-interleaving
+% reinterleavedBlocks = cell(1, numel(correctedBlocks));
+% for i = 1:numel(correctedBlocks)
+%     correctedBlock = correctedBlocks{i};
+%     reinterleavedBlock = zeros(1, ReedSolomon.interleavingDepth*ReedSolomon.messageLength, 'uint8');
+%     idx = 1;
+%     for col = 1:ReedSolomon.messageLength
+%         for row = 1:ReedSolomon.interleavingDepth
+%             reinterleavedBlock(idx) = correctedBlock(row, col);
+%             idx = idx + 1;
+%         end
+%     end
+%     bits = de2bi(reinterleavedBlock, 8, 'left-msb');
+%     bits = reshape(bits.', 1, []);
+%     reinterleavedBlocks{i} = logical(bits(:));
+% end
