eb78484b08ae2328693254cdc671eb1ca200e9fb
tony <anton.dilg.tub@gmail.com>
2025-10-15 13:07:43 +0200

    small changes

--- extraction.m
+++ extraction.m
@@ -11,56 +11,65 @@ sync = uint8([0x1A 0xCF 0xFC 0x1D]);
 mask = (data(1:end-3)==sync(1)) & (data(2:end-2)==sync(2)) & ...
        (data(3:end-1)==sync(3)) & (data(4:end)==sync(4));
 sync_idx = find(mask);
-frame_len = 1024;
-num_frames = numel(sync_idx);
+caduLength = 1024;
+numCadus = numel(sync_idx);
 
-frames = zeros(num_frames,frame_len,'uint8');
-for i = 1:num_frames
+cadus = zeros(numCadus,caduLength,'uint8');
+for i = 1:numCadus
     s = sync_idx(i);
-    if s + frame_len - 1 <= numel(data)
-        frames(i,:) = data(s:s+frame_len-1);
+    if s+caduLength-1 <= numel(data)
+        cadus(i,:) = data(s:s+caduLength-1);
     end
 end
+
+cvcdus = cadus(:,5:end);
+vcdus = cvcdus(:,1:end-128);
 
 %% Pointer Calculation 
-pointer_raw = double(frames(:,13))*256 + double(frames(:,14));
-pointer = bitand(pointer_raw, 2047); 
-
-if verif
-    fprintf('Loaded %d frames of %d bytes each\n', num_frames, frame_len);
-    fprintf('First 4 bytes (ASM): %s\n', sprintf('%02X ', frames(1,1:4)));
-    fprintf('Pointer range: min=%d, max=%d\n', min(pointer), max(pointer));
-end
-
-%% M-PDU Extraction 
-m_pdu_data = {};
-apid_list  = [];
-
-for i = 1:num_frames
-    lo = 17; 
-    hi = frame_len - 5;  
-
-    found = false;
-    for p = lo:hi
-        b1 = frames(i,p); b2 = frames(i,p+1); b3 = frames(i,p+2); b4 = frames(i,p+3); b5 = frames(i,p+4); b6 = frames(i,p+5);
-
-        version = bitshift(b1, -5);  
-        if version ~= 0, continue; end
-
-        apid = bitand( bitor( bitshift(uint16(b1),8), uint16(b2) ), uint16(2047) ); % 11-bit
-        if ~(ismember(apid, [64 65 66 67 68 70 71]))
-            continue;
-        end
-
-
-        pkt_len = double( bitshift(uint16(b5),8) + uint16(b6) ) + 7; 
-        if p + pkt_len - 1 > frame_len
-            continue; 
-        end
-
-        m_pdu_data{end+1} = frames(i, p : p + pkt_len - 1);
-        apid_list(end+1)  = apid;
-        found = true;
-        break;
-    end
-end
+% pointer_raw = double(cadus(:,13))*256 + double(cadus(:,14));
+% pointer = bitand(pointer_raw, 2047); 
+% 
+% if verif
+%     fprintf('Loaded %d frames of %d bytes each\n', num_frames, frame_len);
+%     fprintf('First 4 bytes (ASM): %s\n', sprintf('%02X ', frames(1,1:4)));
+%     fprintf('Pointer range: min=%d, max=%d\n', min(pointer), max(pointer));
+% end
+% 
+% %% M-PDU Extraction 
+% m_pdu_data = {};
+% apid_list  = [];
+% 
+% for i = 1:caduLength
+%     lo = 17; 
+%     hi = frame_len - 5;  
+% 
+%     found = false;
+%     for p = lo:hi
+%         b1 = frames(i,p); b2 = frames(i,p+1); b3 = frames(i,p+2); b4 = frames(i,p+3); b5 = frames(i,p+4); b6 = frames(i,p+5);
+% 
+%         version = bitshift(b1, -5);  
+%         if version ~= 0, continue; end
+% 
+%         apid = bitand( bitor( bitshift(uint16(b1),8), uint16(b2) ), uint16(2047) ); % 11-bit
+%         if ~(ismember(apid, [64 65 66 67 68 70 71]))
+%             continue;
+%         end
+% 
+% 
+%         pkt_len = double( bitshift(uint16(b5),8) + uint16(b6) ) + 7; 
+%         if p + pkt_len - 1 > frame_len
+%             continue; 
+%         end
+% 
+%         m_pdu_data{end+1} = frames(i, p : p + pkt_len - 1);
+%         apid_list(end+1)  = apid;
+%         found = true;
+%         break;
+%     end
+% end
+% 
+% if verif
+%     fprintf('Extracted %d M-PDU packets\n', numel(m_pdu_data));
+%     unique_apids = unique(apid_list);
+%     fprintf('Unique APIDs found: %s\n', sprintf('%d ', unique_apids));
+% end
