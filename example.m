ab5cbdd51d2ad10b1243029fef57a8ff87452d45
anton-vdg <anton.dilg.tub@gmail.com>
2025-10-24 16:46:10 +0200

    corrected huffman decoding

--- example.m
+++ example.m
@@ -86,8 +86,8 @@ for i = 1:4
     end
 end
 
-for i = 1:numel(mcusSorted)
-    for j = 1:numel(mcusSorted{i})
+for i = 1:1%numel(mcusSorted)
+    for j = 1:1%numel(mcusSorted{i})
         mcu = mcusSorted{i}{j};
         pos = 1;
 
@@ -115,11 +115,21 @@ for i = 1:numel(mcusSorted)
             for k = 1:min(16, numel(mcu)-pos+1)
                 key = sprintf('%d', mcu(pos:pos+k-1));
                 if isKey(ACMap.symbols,key)
-                    if ACMap.symbols(key) == 0, break; end
+                    if strcmp(ACMap.symbols(key), '0/0') % EOB
+                        break; 
+                    elseif strcmp(ACMap.symbols(key), '15/0') % ZRL 
+                        acCount = acCount + 16;
+                        pos = pos + 11;
+                        found = true;
+                        break;
+                    end
+                    
                     if pos+k+double(ACMap.symbols(key))-1 > numel(mcu)
                         break;  % Ende erreicht
                     end
-                    nextSymbolLength = double(ACMap.symbols(key));
+                    runsize = str2double(split(ACMap.symbols(key), '/'));
+                    acCount = acCount + runsize(1);
+                    nextSymbolLength = runsize(2);
                     bitArray = mcu(pos+k:pos+k+nextSymbolLength-1);
                     acMagnitudes(acCount) = decodeMagnitude(nextSymbolLength, bitArray);
                     acCount = acCount + 1;
@@ -130,7 +140,43 @@ for i = 1:numel(mcusSorted)
 
             end
             if ~found, break; end
+            if ACMap.symbols(key) == 240, continue; end
         end
         magnitudes{i}{j} = [dcMagnitude, acMagnitudes];
     end
 end
+
+magnitudes = magnitudes{1}{1};
+
+DCT.zigzagTable = [
+     0  1  5  6 14 15 27 28;
+     2  4  7 13 16 26 29 42;
+     3  8 12 17 25 30 41 43;
+     9 11 18 24 31 40 44 53;
+    10 19 23 32 39 45 52 54;
+    20 22 33 38 46 51 55 60;
+    21 34 37 47 50 56 59 61;
+    35 36 48 49 57 58 62 63];
+
+DCT.quantizationTable = [
+16 11 10 16 24 40 51 61;
+12 12 14 19 26 58 60 55;
+14 13 16 24 40 57 69 56;
+14 17 22 29 51 87 80 62;
+18 22 37 56 68 109 103 77;
+24 35 55 64 81 104 113 92;
+49 64 78 87 103 121 120 101;
+72 92 95 98 112 100 103 99];
+
+zigzag = zeros(8,8);
+
+for idx = 0:63
+    [r, c] = find(DCT.zigzagTable == idx);
+    zigzag(r,c) = magnitudes(idx+1);
+end
+
+
+zigzagQuant = zigzag .* DCT.quantizationTable;
+
+iDCT = idct2(zigzagQuant);
+imshow(iDCT)
--- huffman.m
+++ huffman.m
@@ -1,5 +1,7 @@
-function [DCMap, ACMap] = huffman(Huffmann)
-    function dict = generateDict(lengths, symbols)
+function [DCMap, ACMap] = huffman(Huffman)
+    function dict = generateDCDict(Huffman)
+        lengths = Huffman.lDc.lengths;
+        symbols = Huffman.lDc.symbols;
         code = 0;            
         k = 1;                
         dict = cell(sum(lengths), 2);
@@ -18,10 +20,32 @@ function [DCMap, ACMap] = huffman(Huffmann)
         end
     end
 
-    dcDict = generateDict(Huffmann.lDc.lengths, Huffmann.lDc.symbols);
-    acDict = generateDict(Huffmann.lAc.lengths, Huffmann.lAc.symbols);
+    function acDict = generateACDict(Huffman)
+        code = 0;
+        idx = 1;
+        acDict = {};
+    
+        for L = 1:16
+            numCodes = Huffman.lAc.lengths(L);
+            for n = 1:numCodes
+                symbol = Huffman.lAc.symbols(idx);
+                idx = idx + 1;
+                run = bitshift(symbol, -4);
+                sizeVal = bitand(symbol, 15);
+                codeWord = dec2bin(code, L);   
+                acDict{end+1,1} = sprintf('%d/%d', run, sizeVal); 
+                acDict{end,2}   = L;                              
+                acDict{end,3}   = codeWord;                       
+                code = code + 1;
+            end
+            code = bitshift(code, 1);
+        end
+    end
+
+    dcDict = generateDCDict(Huffman);
+    acDict = generateACDict(Huffman);
     DCMap.lengths = containers.Map(dcDict(:,1), dcDict(:,2));
     DCMap.symbols = containers.Map(dcDict(:,2), dcDict(:,1));
-    ACMap.lengths = containers.Map(acDict(:,1), acDict(:,2));
-    ACMap.symbols = containers.Map(acDict(:,2), acDict(:,1));
+    ACMap.rs = containers.Map(acDict(:,1), acDict(:,3));
+    ACMap.symbols = containers.Map(acDict(:,3), acDict(:,1));
 end
